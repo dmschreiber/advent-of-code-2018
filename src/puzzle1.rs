78fcff0bd17815bce57c4248340a4d56fab2ec47
Dan Schreiber <dmschreiber@gmail.com>
2021-01-08 10:12:48 -0500

    prod and test data tests

--- src/puzzle1.rs
+++ src/puzzle1.rs
@@ -2,16 +2,23 @@
 mod tests {
   use crate::common;
   #[test]
-  pub fn puzzle1() {
+  pub fn puzzle1_test() {
     assert!(common::format_binary(10)=="1010");
+    super::solve("./inputs/puzzle1-test.txt".to_string());
+  }
+
+  #[test]
+  pub fn puzzle1_prod() {
+    assert!(common::format_binary(10)=="1010");
+    super::solve("./inputs/puzzle1.txt".to_string());
   }
 }
 
 use crate::common;
 use std::convert::TryInto;
 
-pub fn solve() -> i64 {
-  let lines = common::read_input("./inputs/puzzle1.txt".to_string());
+pub fn solve(file_name : String) -> i64 {
+  let lines = common::read_input(file_name);
 
   let map = common::make_map(&lines);
   let spot = common::get_spot_on_map(&map, 0, 0, '.');
