2ec3972bf8699c77365abc166f1db28934500c4e
Dan Schreiber <dmschreiber@gmail.com>
2021-01-10 21:56:29 -0500

    day 7 cleanup

--- src/puzzle7.rs
+++ src/puzzle7.rs
@@ -61,6 +61,7 @@ fn load_all_steps(lines : Vec<String>) -> HashMap<char,Vec<char>> {
 
   return list;
 }
+
 pub fn find_order(file_name : String) -> String {
   let ret_val;
   let lines = common::read_input(file_name);
@@ -68,31 +69,22 @@ pub fn find_order(file_name : String) -> String {
   let mut list = load_all_steps(lines);
 
   let mut available_chars = list.keys().filter(|c| get_prerequ(**c, &list).len() == 0).map(|c| *c).collect::<Vec<char>>();
-
   let mut resulting_order : Vec<char>= vec![];
 
   while available_chars.len() > 0 {
     available_chars.sort();
 
-    let mut char_index = 0;
-    let mut next_char = available_chars[char_index];
-    while get_prerequ(next_char, &list).iter().filter(|target_char| !resulting_order.contains(target_char)).count() > 0 {
-      // println!("Prerequ of {} are {:?}", next_char, get_prerequ(next_char, &list));
-      char_index += 1;
-      next_char = available_chars[char_index].clone();
-    }
+    let next_char = available_chars.iter().filter(|next_char| get_prerequ(**next_char, &list).iter().filter(|target_char| !resulting_order.contains(target_char)).count() == 0).map(|c| *c).collect::<Vec<char>>()[0];
+    let char_index = available_chars.iter().position(|&c| c == next_char).unwrap();
     available_chars.remove(char_index);
 
     resulting_order.push(next_char);
-    let subsequent;
+
     if let Some(thing) = list.get_mut(&next_char) {
-      subsequent = thing.clone();
-    } else {
-      subsequent = vec![];
-    }
-    for subsequent_char in subsequent {
-      if !resulting_order.contains(&&subsequent_char) {
-        available_chars.push(subsequent_char);
+      for subsequent_char in thing {
+        if !resulting_order.contains(&&subsequent_char) {
+          available_chars.push(*subsequent_char);
+        }
       }
     }
 
@@ -126,16 +118,18 @@ fn solve_part2(file_name : String, num_workers : usize, initial_time : u8) -> i6
         // decrement
         *w = *w - 1;
       } else if steps_remain.len() > 0 {
-        let mut char_index = 0;
-        while get_prerequ(steps_remain[char_index] as char, &steps_map).iter().filter(|pre_char| steps_remain.contains(&(**pre_char)) || work.contains(pre_char)).count() > 0 {
-          char_index = char_index + 1;
-          if char_index == steps_remain.len() { break; }
-        }
-        if char_index < steps_remain.len() {
-          *workers.get_mut(position).unwrap() = Some(( initial_time + steps_remain[char_index] as u8 - b'A' ) as u32);
-          work[position] = steps_remain[char_index];
+        // let mut char_index = 0;
+        let upcoming_candidates = steps_remain.iter()
+                                  .filter(|which_char| get_prerequ(**which_char as char, &steps_map).iter()
+                                                                .filter(|pre_char| steps_remain.contains(&(**pre_char)) || work.contains(pre_char)).count() == 0)
+                                  .map(|c| *c).collect::<Vec<char>>();
+
+        if upcoming_candidates.len() > 0 {
+          let good_candidate = upcoming_candidates[0];
+          let char_index = steps_remain.iter().position(|&c| c == good_candidate).unwrap();
+          *workers.get_mut(position).unwrap() = Some(( initial_time + good_candidate as u8 - b'A' ) as u32);
+          work[position] = good_candidate;
           steps_remain.remove(char_index);
-          // assign work
         }
       }
     }
@@ -150,9 +144,6 @@ fn solve_part2(file_name : String, num_workers : usize, initial_time : u8) -> i6
     current_minute = current_minute + 1;
   }
  
-  // 1007 too low
-  // 1070 too high
-
   return current_minute.try_into().unwrap();
 }
 pub fn solve(file_name : String) -> i64 {
