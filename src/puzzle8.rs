28ab3769d3dfc42f1d5cc479ff3c71bf04e4ecac
Dan Schreiber <dmschreiber@gmail.com>
2021-01-11 16:16:23 -0500

    cleanup

--- src/puzzle8.rs
+++ src/puzzle8.rs
@@ -7,9 +7,9 @@ mod tests {
     // println!("{}",super::solve("./inputs/puzzle8-test.txt".to_string()));
     println!("{:?}", super::sum_metadata(vec![2,3,0,3,10,11,12,1,2,1,1,0,1,1,1,99,2,1,1,2]));
     println!("{:?}", super::sum_metadata(vec![2,3,1,3,0,1,100,10,11,12,1,1,0,1,99,2,1,1,2]));
-    assert!(138==super::solve("./inputs/puzzle8-test.txt".to_string()));
+    assert!(138==super::solve_part1(&"./inputs/puzzle8-test.txt".to_string()));
     println!("Part 1 result {}", super::solve("./inputs/puzzle8-test.txt".to_string()));
-    assert!(66==super::solve_part2("./inputs/puzzle8-test.txt".to_string()));
+    assert!(66==super::solve_part2(&"./inputs/puzzle8-test.txt".to_string()));
     println!("{:?}", super::sum_value(vec![2,3,1,3,0,1,100,10,11,12,1,1,0,1,99,2,1,1,2]));
   }
 
@@ -17,15 +17,12 @@ mod tests {
   pub fn puzzle8_prod() {
     assert!(common::format_binary(10)=="1010");
     println!("Part 1 result {}", super::solve("./inputs/puzzle8.txt".to_string()));
-    assert!(49602==super::solve("./inputs/puzzle8.txt".to_string()));
-    println!("Part 2 result {}", super::solve_part2("./inputs/puzzle8.txt".to_string()));
+    assert!(49602==super::solve_part1(&"./inputs/puzzle8.txt".to_string()));
+    println!("Part 2 result {}", super::solve_part2(&"./inputs/puzzle8.txt".to_string()));
   }
 }
 
 use crate::common;
-use std::convert::TryInto;
-use std::time::Duration;
-use std::thread;
 
 fn sum_value(nums : Vec<u32>) -> (usize,i64) {
   if nums.len() == 0 {
@@ -76,16 +73,11 @@ fn sum_metadata(nums : Vec<u32>) -> (usize,i64) {
     if nums.len() <= nums[1] as usize { panic!("length vs metadat {}, {}", nums.len(), nums[1]); }
     let end = nums.len() - nums[1] as usize;
     
-    // println!("Calling starting with offset {} end {} - {:?}", offset, end, nums);
-    // while offset < end {
-    for i in 0..nums[0] {
-        // println!("Calling sum_metadata {} {} - running total {} {:?}", offset, end, intermediate_total, nums);      
+    for _i in 0..nums[0] {
         let (consumed_offset,contributing_total) = sum_metadata(nums[offset..end].to_vec());
 
         intermediate_total = intermediate_total + contributing_total;
         offset  = offset+consumed_offset;
-        // if offset != end { println!("more work to be done"); }
-        // println!("Complete sum_metadata {} ({} consumed) {}", offset, consumed_offset, end);      
 
     }
 
@@ -94,9 +86,8 @@ fn sum_metadata(nums : Vec<u32>) -> (usize,i64) {
   }
 }
 
-pub fn solve_part2(file_name : String) -> i64 {
-  let lines = common::read_input(file_name);
-  let mut retval = 0;
+pub fn solve_part2(file_name : &String) -> i64 {
+  let lines = common::read_input(file_name.to_string());
 
   let nums = lines[0].split(' ').map(|n| n.parse::<u32>().unwrap()).collect::<Vec<u32>>();
   println!("Start solve part 2 with {} nums", nums.len());
@@ -107,9 +98,8 @@ pub fn solve_part2(file_name : String) -> i64 {
 
 }
 
-pub fn solve(file_name : String) -> i64 {
-  let lines = common::read_input(file_name);
-  let mut retval = 0;
+pub fn solve_part1(file_name : &String) -> i64 {
+  let lines = common::read_input(file_name.to_string());
 
   let nums = lines[0].split(' ').map(|n| n.parse::<u32>().unwrap()).collect::<Vec<u32>>();
   println!("Start solve with {} nums", nums.len());
@@ -117,5 +107,9 @@ pub fn solve(file_name : String) -> i64 {
   let (used,total) = sum_metadata(nums);
   println!("{} used sum is {}", used, total);
   return total;
-  
+
+}
+pub fn solve(file_name : String) -> i64 {
+  solve_part1(&file_name);
+  return solve_part2(&file_name);  
 }
