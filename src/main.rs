29ea0d8ffada78b63784a1d46b0cd3ab213e56ec
Dan Schreiber <dmschreiber@gmail.com>
2021-01-12 16:26:58 -0500

    Day 10 base

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod common;
 pub mod puzzle1;
+pub mod puzzle10;
 pub mod puzzle9;
 pub mod puzzle8;
 pub mod puzzle7;
@@ -25,6 +26,7 @@ async fn main() {
 
     let targets : Vec<(fn(std::string::String) -> i64,String)> = vec![
         (puzzle1::solve,"./inputs/puzzle1.txt".to_string()),
+        (puzzle10::solve,"./inputs/puzzle10.txt".to_string()),
         (puzzle9::solve,"428 players; last marble is worth 70825 points".to_string()),
         (puzzle8::solve,"./inputs/puzzle8.txt".to_string()), // 2+ hrs
         (puzzle7::solve,"./inputs/puzzle7.txt".to_string()), // 2+ hrs
