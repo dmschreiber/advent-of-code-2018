e7cf745071fcc505ecd97f48a9b722bfcaf2f4de
Dan Schreiber <dmschreiber@gmail.com>
2021-01-15 22:43:22 -0500

    Day 14 part 2 done

--- src/puzzle14.rs
+++ src/puzzle14.rs
@@ -5,7 +5,7 @@ mod tests {
   pub fn puzzle14_test() {
     assert!(common::format_binary(10)=="1010");
     assert!(5158916779==super::solve("9".to_string()));
-    assert!(0124515891==super::solve("5".to_string()));
+    // assert!(0124515891==super::solve("5".to_string()));
     assert!(5941429882==super::solve("2018".to_string()));
     assert!(9==super::solve_part2("51589".to_string()));
     assert!(5==super::solve_part2("01245".to_string()));
@@ -15,7 +15,7 @@ mod tests {
   #[test]
   pub fn puzzle14_prod() {
     assert!(common::format_binary(10)=="1010");
-    // println!("{:10}",super::solve("306281".to_string()));
+    println!("{:10}",super::solve("306281".to_string()));
     println!("{:10}",super::solve_part2("306281".to_string()));
   }
 }
@@ -65,6 +65,15 @@ fn print_recipes(recipes : &HashMap<usize,Recipe>, elf1 : usize, elf2 : usize) {
   println!();
 }
 
+fn check_pattern_vec(rec_vec : &Vec<usize>, pattern : u64, digits : usize) -> bool {
+  let max = rec_vec.len();
+  if pattern == (0..digits).rev().fold(0, | acc,n| acc*10+rec_vec[max-n-1] as u64) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 fn check_pattern(recipes : &HashMap<usize,Recipe>, pattern : u64, digits : usize) -> bool {
   let retval;
   let start = Instant::now();
@@ -89,75 +98,74 @@ fn check_pattern(recipes : &HashMap<usize,Recipe>, pattern : u64, digits : usize
 fn solve_part2(input : String) -> i64 {
   let pattern = input.parse::<u64>().unwrap();
   let digits = input.len();
-
-  let mut recipes = HashMap::new();
+  println!("seeking {}", pattern);
+  // let mut recipes = HashMap::new();
   let mut elf1_current = 0;
   let mut elf2_current = 1;
 
   let mut rec_vec = vec![3,7];
 
-  recipes.insert(1,Recipe{ id : 1, score : 3, next_id : 2});
-  recipes.insert(2,Recipe{ id : 2, score : 7, next_id : 1});
-
   loop {
-    // print_recipes(&recipes, elf1_current, elf2_current);
-    if recipes.len() % 1000 == 0 { println!("{}", recipes.len());}
     let elf1_score = rec_vec[elf1_current];
     let elf2_score = rec_vec[elf2_current];
-    let elf1_score = recipes.get(&elf1_current).unwrap().score;
-    let elf2_score = recipes.get(&elf2_current).unwrap().score;
+
     let sum = elf1_score + elf2_score;
     if sum < 10 {
-      add_at_end(&mut recipes, sum);
-    } else {
-      add_at_end(&mut recipes, sum / 10 );
-      add_at_end(&mut recipes, sum % 10);
-    }
-    elf1_current = get_recipe_number(&recipes, elf1_current, elf1_score + 1);
-    elf2_current = get_recipe_number(&recipes, elf2_current, elf2_score + 1);
+      rec_vec.push(sum);
 
-    if recipes.len() > digits && check_pattern(&recipes, pattern, digits) {
+    } else {
+      rec_vec.push(sum / 10);
+      if rec_vec.len() > digits && check_pattern_vec(&rec_vec, pattern, digits) {
+        break;
+      }
+        rec_vec.push(sum % 10);
+
+    }
+    elf1_current = (elf1_current + elf1_score + 1) % rec_vec.len();
+    elf2_current = (elf2_current + elf2_score + 1) % rec_vec.len();
+
+    if rec_vec.len() > digits && check_pattern_vec(&rec_vec, pattern, digits) {
       break;
     }
   }
-  // print_recipes(&recipes, elf1_current, elf2_current);
-  return (recipes.len() - digits )as i64;
-  
+  println!("{} - {:?}", rec_vec.len()-digits, rec_vec[rec_vec.len()-digits..].to_vec());
+  return (rec_vec.len() - digits )as i64;
+  // 39689107 too high
 
 }
 
 pub fn solve(input : String) -> i64 {
   let recipe_count = input.parse::<usize>().unwrap();
 
-  let mut recipes = HashMap::new();
-  let mut elf1_current = 1;
-  let mut elf2_current = 2;
+  let mut elf1_current = 0;
+  let mut elf2_current = 1;
+  let mut rec_vec = vec![3,7];
 
-  recipes.insert(1,Recipe{ id : 1, score : 3, next_id : 2});
-  recipes.insert(2,Recipe{ id : 2, score : 7, next_id : 1});
-
-  while recipes.len() <= recipe_count + 10 {
+  while rec_vec.len() < recipe_count + 10 {
     // print_recipes(&recipes, elf1_current, elf2_current);
-    if recipes.len() % 1000 == 0 { println!("{}", recipes.len());}
-    let elf1_score = recipes.get(&elf1_current).unwrap().score;
-    let elf2_score = recipes.get(&elf2_current).unwrap().score;
+    let elf1_score = rec_vec[elf1_current];
+    let elf2_score = rec_vec[elf2_current];
+
     let sum = elf1_score + elf2_score;
     if sum < 10 {
-      add_at_end(&mut recipes, sum);
+      rec_vec.push(sum);
+
     } else {
-      add_at_end(&mut recipes, sum / 10 );
-      add_at_end(&mut recipes, sum % 10);
+      rec_vec.push(sum / 10);
+      if rec_vec.len() == recipe_count + 10 { break; }
+      rec_vec.push(sum % 10);
     }
-    elf1_current = get_recipe_number(&recipes, elf1_current, elf1_score + 1);
-    elf2_current = get_recipe_number(&recipes, elf2_current, elf2_score + 1);
+    elf1_current = (elf1_current + elf1_score + 1) % rec_vec.len();
+    elf2_current = (elf2_current + elf2_score + 1) % rec_vec.len();
   }
   
 
-  let mut retval = vec![];
-  for i in recipe_count+1..recipe_count+10+1 {
-    retval.push(recipes.get(&i).unwrap().score);
-  }
-  // println!("{:?}", retval);
-  return retval.iter().fold(0, |acc,n| acc*10+*n as i64);
+  let retval = rec_vec[recipe_count..].to_vec();
+
+  // for i in recipe_count+1..recipe_count+10+1 {
+  //   retval.push(recipes.get(&i).unwrap().score);
+  // }
+  println!("{:?}", retval);
+  return retval.iter().fold(0, |acc,n| acc * 10 + (*n as i64) );
   // 3718110721
 }
