270a7933dab3edf22c01b607c9c1da97294ba4b2
Dan Schreiber <dmschreiber@gmail.com>
2021-01-15 22:46:06 -0500

    Day 14 cleanup

--- src/puzzle14.rs
+++ src/puzzle14.rs
@@ -5,7 +5,6 @@ mod tests {
   pub fn puzzle14_test() {
     assert!(common::format_binary(10)=="1010");
     assert!(5158916779==super::solve("9".to_string()));
-    // assert!(0124515891==super::solve("5".to_string()));
     assert!(5941429882==super::solve("2018".to_string()));
     assert!(9==super::solve_part2("51589".to_string()));
     assert!(5==super::solve_part2("01245".to_string()));
@@ -20,49 +19,12 @@ mod tests {
   }
 }
 
-use crate::common;
-use std::convert::TryInto;
-use std::collections::HashMap;
-use std::time::Instant;
 
 #[derive(Debug,Clone,PartialEq)]
 pub struct Recipe {
   id : usize,
   score : usize,
   next_id : usize,
-}
-
-fn get_recipe_number(recipes : &HashMap<usize,Recipe>, current : usize, steps : usize) -> usize {
-  let mut current = recipes.get(&current).unwrap();
-  for i in 0..steps {
-    let next_id = current.next_id;
-    current = recipes.get(&next_id).unwrap();
-  }
-  return current.id;
-}
-
-fn add_at_end(recipes : &mut HashMap<usize,Recipe>, sum : usize) {
-  let target = *recipes.keys().max().unwrap();
-  let mut r = recipes.get_mut(&target).unwrap();
-  let last_next_id = r.next_id;
-  r.next_id = target+1;
-  recipes.insert(target+1, Recipe{ id : target+1, score : sum, next_id : last_next_id});
-}
-
-fn print_recipes(recipes : &HashMap<usize,Recipe>, elf1 : usize, elf2 : usize) {
-  let mut r = 1;
-  loop {
-    if r == elf1 {
-      print!("({}) ", recipes.get(&r).unwrap().score);
-    } else if r == elf2 {
-      print!("[{}] ", recipes.get(&r).unwrap().score);
-    } else {
-      print!("{} ", recipes.get(&r).unwrap().score);
-    }
-    r = recipes.get(&r).unwrap().next_id;
-    if r == 1 { break; }
-  }
-  println!();
 }
 
 fn check_pattern_vec(rec_vec : &Vec<usize>, pattern : u64, digits : usize) -> bool {
@@ -74,26 +36,6 @@ fn check_pattern_vec(rec_vec : &Vec<usize>, pattern : u64, digits : usize) -> bo
   }
 }
 
-fn check_pattern(recipes : &HashMap<usize,Recipe>, pattern : u64, digits : usize) -> bool {
-  let retval;
-  let start = Instant::now();
-  let max = recipes.len();
-  // println!("{:?}", start.elapsed());
-  if pattern % 10 != recipes.get(&max).unwrap().score as u64 
-  { 
-    retval= false; 
-  } else if (pattern / 10) % 10 != recipes.get(&(max-1)).unwrap().score as u64 {
-    retval= false; 
-
-  }
-  else if pattern == (0..digits).rev().fold(0, |acc,n| acc*10+recipes.get(&(max-n)).unwrap().score as u64) {
-    retval= true;
-  } else {
-    retval=false;
-  }
-  // println!("{:?}", start.elapsed());
-  return retval;
-}
 
 fn solve_part2(input : String) -> i64 {
   let pattern = input.parse::<u64>().unwrap();
@@ -162,10 +104,7 @@ pub fn solve(input : String) -> i64 {
 
   let retval = rec_vec[recipe_count..].to_vec();
 
-  // for i in recipe_count+1..recipe_count+10+1 {
-  //   retval.push(recipes.get(&i).unwrap().score);
-  // }
+
   println!("{:?}", retval);
   return retval.iter().fold(0, |acc,n| acc * 10 + (*n as i64) );
-  // 3718110721
 }
