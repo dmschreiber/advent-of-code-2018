10cb553f839be90daf5df0c52e2b0fe2dad103ca
Dan Schreiber <dmschreiber@gmail.com>
2021-01-23 17:27:02 -0500

    Day 19 cleanup

--- src/puzzle19.rs
+++ src/puzzle19.rs
@@ -3,8 +3,7 @@ mod tests {
   use crate::common;
   #[test]
   pub fn puzzle19_test() {
-    assert!(common::format_binary(10)=="1010");
-    super::solve("./inputs/puzzle19-test.txt".to_string());
+
   }
 
   #[test]
@@ -22,6 +21,48 @@ use crate::puzzle16;
 lazy_static! {
   static ref IP_REGEX: Regex = Regex::new(r"^#ip ([0-9+]+)$").unwrap();
   static ref OPCODE_REGEX: Regex = Regex::new(r"^([a-z]+) ([0-9+]+) ([0-9+]+) ([0-9+]+)$").unwrap();
+}
+
+pub fn run (opcodes : &Vec<Vec<usize>>, initial_state : Vec<usize>, ip_register : usize) -> Vec<usize> {
+  let mut ip = 0;
+  let mut state = initial_state.clone();
+
+  loop {
+    let o = &opcodes[ip];
+    //   // R1 = 1;
+    //   // R4 = 1;
+    //   // loop {
+    //     // if R1*R4 == R5 {
+    //     // R0 = R0 + R1;
+    //     // } else {
+    //     // R4 = R4 + 1; 
+    //     // if R4 > R5 break;
+    //     // }
+    //   // }
+
+    if ip == 2 && state[1] != 0 {
+      for i in 1..=state[5] {
+        state[1] = i;
+        if state[5] % state[1] == 0 {
+          state[0] = state[0] + state[1];
+        }  
+        state[4] = state[5];
+        // state[1] = state[1] + 1;
+        state[ip_register] = 15;
+        ip = 15;
+
+      }
+    }
+
+    state[ip_register] = ip;
+    state = puzzle16::op_code(o[0], o[1..].to_vec(), &state);
+    ip = state[ip_register];
+    ip = ip + 1;
+    state[ip_register] = ip;
+
+    if ip >= opcodes.len() {break; }
+  }
+  return state;
 }
 
 pub fn solve(file_name : String) -> i64 {
@@ -52,72 +93,18 @@ pub fn solve(file_name : String) -> i64 {
   }
 
   let mut state = vec![0; 6];
-  let mut ip = 0;
-  let mut counter = 0;
 
-  loop {
-    let o = &opcodes[ip];
-    state[ip_register] = ip;
-    state = puzzle16::op_code(o[0], o[1..].to_vec(), &state);
-    ip = state[ip_register];
-    ip = ip + 1;
-    state[ip_register] = ip;
-    counter = counter + 1;
-    if ip >= opcodes.len() {break; }
-  }
-    println!("Day 19 part 1 {:?} ({})", state[0], counter);
+  state = run(&opcodes,state,ip_register);
+
+    println!("Day 19 part 1 {:?}", state[0]);
 
     let mut state = vec![0; 6];
     state[0] = 1;
-    let mut ip = 0;
-    let mut counter = 0;
+    state = run(&opcodes, state, ip_register);
 
-    loop {
-      // print!("ip={}, {:?} ", ip, state);
-      let o = &opcodes[ip];
-
-      // R1 = 1;
-      // R4 = 1;
-      // loop {
-        // if R1*R4 == R5 {
-        // R0 = R0 + R1;
-        // } else {
-        // R4 = R4 + 1; 
-        // if R4 > R5 break;
-        // }
-      // }
-      // 10551367 + 1 + 2801 + 3767
+    //   // 10551367 + 1 + 2801 + 3767
 
 
-      if ip == 2 && state[1] == 1 {
-        state[0] = state[0] + 1;
-        state[1] = 2;
-        state[4] = state[5];
-        state[ip_register] = 12;
-        ip = 12;
-
-      }
-      else if ip == 2 && state[1] != 0 {
-        // println!("{:?}", state);
-        if state[5] % state[1] == 0 {
-          state[0] = state[0] + state[1];
-        }  
-        state[4] = state[5];
-        state[1] = state[1] + 1;
-        state[ip_register] = 12;
-        ip = 12;
-      }
-
-      state[ip_register] = ip;
-      state = puzzle16::op_code(o[0], o[1..].to_vec(), &state);
-      ip = state[ip_register];
-      ip = ip + 1;
-      state[ip_register] = ip;
-
-      counter = counter + 1;
-      if ip >= opcodes.len() { break; }
-      // if counter > 7484592 { break; }
-    }     
     println!("Day 19 part 2 {:?}", state[0]);
     // 10551368 too low
     return state[0].try_into().unwrap();
