b23ce598af898b908f7c384999b48ea2f4fdee6d
Dan Schreiber <dmschreiber@gmail.com>
2021-02-01 16:58:12 -0500

    Day 22 prep

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod common;
 pub mod puzzle1;
+pub mod puzzle22;
 pub mod puzzle21;
 pub mod puzzle20;
 pub mod puzzle19;
@@ -37,6 +38,7 @@ async fn main() {
 
     let targets : Vec<(fn(std::string::String) -> i64,String)> = vec![
         (puzzle1::solve,"./inputs/puzzle1.txt".to_string()),
+        (puzzle22::solve,"./inputs/puzzle22.txt".to_string()),
         (puzzle21::solve,"./inputs/puzzle21.txt".to_string()),
         (puzzle20::solve,"./inputs/puzzle20.txt".to_string()),
         (puzzle19::solve,"./inputs/puzzle19.txt".to_string()),
---
+++ src/puzzle22.rs
@@ -0,0 +1,32 @@
+#[cfg(test)]
+mod tests {
+  use crate::common;
+  #[test]
+  pub fn puzzle22_test() {
+    assert!(common::format_binary(10)=="1010");
+    super::solve("./inputs/puzzle22-test.txt".to_string());
+  }
+
+  #[test]
+  pub fn puzzle22_prod() {
+    assert!(common::format_binary(10)=="1010");
+    super::solve("./inputs/puzzle22.txt".to_string());
+  }
+}
+
+use crate::common;
+use std::convert::TryInto;
+use std::time::Duration;
+use std::thread;
+
+pub fn solve(file_name : String) -> i64 {
+  let lines = common::read_input(file_name);
+  println!("Start solve");
+  thread::sleep(Duration::from_secs(10));
+
+  let map = common::make_map(&lines);
+  let spot = common::get_spot_on_map(&map, 0, 0, '.');
+  assert!(spot == '.');
+  
+  return 0.try_into().unwrap();
+}
